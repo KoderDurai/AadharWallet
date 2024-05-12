e694e5fba2e6ec6e448c773986050139348c9f57
Karthikeyan Duriavel <75940774+KoderDurai@users.noreply.github.com>
2024-05-12 01:25:39 +0100

    Initial Commit

---
+++ AadharWallet/AadharWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  AadharWalletApp.swift
+//  AadharWallet
+//
+//  Created by Karthikeyan Duraivel on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AadharWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
